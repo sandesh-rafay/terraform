2731d1314742752e19675daf8d304e7e3366c01d
sandesh-rafay <sandesh@rafay.co>
2025-10-28 21:22:55 +0530

    eks variables

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,8 +1,21 @@
-variable "cluster_name" {
-  type        = string
-  description = "Name of EKS cluster"
-}
 variable "region" {
   type        = string
   default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  type        = string
+  sensitive   = true
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  sensitive   = true
+  description = "AWS Secret Key"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS Cluster Name"
 }
