0e08cf5b37a95bf1d5669c13ad6cbb8036480318
sandesh-rafay <sandesh@rafay.co>
2025-10-28 20:15:40 +0530

    removed ' from keys variable

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.region
-  access_key = "var.aws_access_key"
-  secret_key = "var.aws_secret_key"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # IAM role for EKS control plane
