507b86c12b3fd357d20b0bca7851cc3d544648ad
sandesh-rafay <sandesh@rafay.co>
2025-10-28 16:31:32 +0530

    Complete main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -2,10 +2,67 @@ provider "aws" {
   region = var.region
 }
 
+# IAM role for EKS control plane
+resource "aws_iam_role" "eks_role" {
+  name = "${var.cluster_name}-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach required managed policies
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+# Basic VPC networking for EKS
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "eks_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
+
+resource "aws_security_group" "eks_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+# The actual EKS cluster
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
-  # … other config …
+
+  vpc_config {
+    subnet_ids         = aws_subnet.eks_subnet[*].id
+    security_group_ids = [aws_security_group.eks_sg.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_service_policy,
+  ]
 }
 
 output "eks_cluster_name" {
--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -3,8 +3,15 @@ provider "aws" {
 }
 
 resource "aws_db_instance" "this" {
-  identifier = var.db_name
-  # … other config …
+  identifier              = var.db_name
+  allocated_storage       = 20
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  username                = "admin"
+  password                = "Admin12345!"
+  parameter_group_name    = "default.mysql8.0"
+  skip_final_snapshot     = true
 }
 
 output "rds_hostname" {
